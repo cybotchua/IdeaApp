5c56dc4c5ba33ba31a3d78b1281035057563cd1f
cybotchua <cybotchua@hotmail.com>
2018-03-23 22:41:37 +0800

    search bar fixed

--- IdeaApp (FinalAssessment)/Idea/IdeaViewController.swift
+++ IdeaApp (FinalAssessment)/Idea/IdeaViewController.swift
@@ -22,11 +22,11 @@ class IdeaViewController: UIViewController {
         }
     }
     
-    @IBOutlet weak var searchBar: UISearchBar! {
-        didSet {
-            searchBar.delegate = self
-        }
-    }
+//    @IBOutlet weak var searchBar: UISearchBar! {
+//        didSet {
+//            searchBar.delegate = self
+//        }
+//    }
     
     @IBOutlet weak var tableView: UITableView! {
         didSet {
@@ -41,6 +41,8 @@ class IdeaViewController: UIViewController {
     
     var ref : DatabaseReference!
     
+    let searchController = UISearchController(searchResultsController: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -51,12 +53,29 @@ class IdeaViewController: UIViewController {
         self.navigationItem.title = "Ideas"
         
         observeIdeas()
+        
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        definesPresentationContext = true
+        tableView.tableHeaderView = searchController.searchBar
+        searchController.searchBar = UIOffset.zero
+        
+        searchController.searchBar.scopeButtonTitles = ["All", "Not Started", "In Progress", "Completed", "Incomplete"]
+        searchController.searchBar.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 //        ideas = []
 //        observeIdeas()
+        tableView.reloadData()
+    }
+    
+    func filterIdeas(searchText: String, scope: String = "All") {
+        filteredIdeas = ideas.filter({ (idea) -> Bool in
+            let statusMatch = (scope == "All") || (idea.status.rawValue == scope)
+            return statusMatch && idea.title.lowercased().contains(searchText.lowercased())
+        })
         tableView.reloadData()
     }
     
@@ -109,15 +128,27 @@ class IdeaViewController: UIViewController {
 extension IdeaViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return filteredIdeas.count
+        
+        if searchController.isActive && searchController.searchBar.text != "" {
+            return filteredIdeas.count
+        }
+        return ideas.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? IdeaTableViewCell else {return UITableViewCell()}
         
-        cell.titleLabel.text = filteredIdeas[indexPath.row].title
-        cell.statusLabel.text = filteredIdeas[indexPath.row].status.rawValue
-        cell.dateLabel.text = filteredIdeas[indexPath.row].date
+        let idea : Idea
+        
+        if searchController.isActive && searchController.searchBar.text != "" {
+            idea = filteredIdeas[indexPath.row]
+        } else {
+            idea = ideas[indexPath.row]
+        }
+        
+        cell.titleLabel.text = idea.title
+        cell.statusLabel.text = idea.status.rawValue
+        cell.dateLabel.text = idea.date
         
         return cell
     }
@@ -128,7 +159,13 @@ extension IdeaViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         guard let vc = storyboard?.instantiateViewController(withIdentifier: "IdeaDetailViewController") as? IdeaDetailViewController else {return}
         
-        let selectedIdea = filteredIdeas[indexPath.row]
+        let selectedIdea : Idea
+        
+        if searchController.isActive && searchController.searchBar.text != "" {
+            selectedIdea = filteredIdeas[indexPath.row]
+        } else {
+            selectedIdea = ideas[indexPath.row]
+        }
         
         vc.selectedIdea = selectedIdea
         
@@ -138,34 +175,55 @@ extension IdeaViewController: UITableViewDelegate {
     
 }
 
-extension IdeaViewController: UISearchBarDelegate {
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        guard !searchText.isEmpty else {
-            filteredIdeas = ideas
-            tableView.reloadData()
-            return
-        }
-
-        filteredIdeas = ideas.filter({ (idea) -> Bool in
-            idea.title.lowercased().contains(searchText.lowercased())
-        })
-        self.tableView.reloadData()
-    }
-    
-    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
-        guard let scopeString = searchBar.scopeButtonTitles?[searchBar.selectedScopeButtonIndex] else {return}
+extension IdeaViewController : UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
         
-        let selectedStatus = Idea.Status(rawValue: scopeString) ?? .notStarted
-        let searchText = searchBar.text ?? ""
+        let searchBar = searchController.searchBar
+        guard let scopeButtonTitles = searchBar.scopeButtonTitles else {return}
+        let scope = scopeButtonTitles[searchBar.selectedScopeButtonIndex]
         
-        filteredIdeas = ideas.filter({ (idea) -> Bool in
-            let isStatusMatching = (idea.status == .notStarted) || (idea.status == selectedStatus)
-            
-            let isMatchingSearchText = idea.title.lowercased().contains(searchText.lowercased()) || searchText.lowercased().count == 0
-            
-            return isStatusMatching && isMatchingSearchText
-        })
-            self.tableView.reloadData()
-        
+        guard let searchText = searchController.searchBar.text else {return}
+        filterIdeas(searchText: searchText, scope: scope)
     }
 }
+
+extension IdeaViewController : UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        guard let searchText = searchBar.text,
+            let scope = searchBar.scopeButtonTitles else {return}
+        filterIdeas(searchText: searchText, scope: scope[selectedScope])
+    }
+}
+
+//extension IdeaViewController: UISearchBarDelegate {
+//    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+//        guard !searchText.isEmpty else {
+//            filteredIdeas = ideas
+//            tableView.reloadData()
+//            return
+//        }
+//
+//        filteredIdeas = ideas.filter({ (idea) -> Bool in
+//            idea.title.lowercased().contains(searchText.lowercased())
+//        })
+//        self.tableView.reloadData()
+//    }
+//    
+//    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+//        guard let scopeString = searchBar.scopeButtonTitles?[searchBar.selectedScopeButtonIndex] else {return}
+//        
+//        let selectedStatus = Idea.Status(rawValue: scopeString) ?? .notStarted
+//        let searchText = searchBar.text ?? ""
+//        
+//        filteredIdeas = ideas.filter({ (idea) -> Bool in
+//            let isStatusMatching = (idea.status == .notStarted) || (idea.status == selectedStatus)
+//            
+//            let isMatchingSearchText = idea.title.lowercased().contains(searchText.lowercased()) || searchText.lowercased().count == 0
+//            
+//            return isStatusMatching && isMatchingSearchText
+//        })
+//            self.tableView.reloadData()
+//        
+//    }
+//}
+
